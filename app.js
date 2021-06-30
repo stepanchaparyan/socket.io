3f144098b065151ab18a42fc831beca6247246bc
stepanchaparyan <stepanchaparyan@gmail.com>
2021-07-01 02:40:05 +0400

    updated env port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('https'); // https deploy on heroku
 const cors = require('cors');
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 
