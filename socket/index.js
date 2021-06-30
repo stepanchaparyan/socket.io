80f07af645e3aafc19b7b9fdc4606207b3a87d1d
stepanchaparyan <stepanchaparyan@gmail.com>
2021-07-01 01:24:55 +0400

    fixed leave chat emit and removed consoles

--- socket/index.js
+++ socket/index.js
@@ -4,19 +4,15 @@ const listen = (server) => {
   const io = socketio(server);
   const roomId = '3TsdhF83PS';
   io.on('connect', (socket) => {
-    console.log('User connected');
     socket.on('join', ({ user }) => {
-      console.log('user joined to the room', user);
       socket.join(roomId);
       socket.to(roomId).emit('userJoined', user);
     });
     socket.on('leaveChat', ({ user }) => {
-      console.log('user left');
       socket.leave(roomId);
-      socket.to(roomId).emit('user left room', user);
+      socket.to(roomId).emit('leaveChat', user);
     });
     socket.on('sendMessage', ({ message }) => {
-      console.log('send message created', message);
       socket.to(roomId).emit('messageReceived', message);
     });
   });      
